38db6d57a8572c89380b27bce11488253378857d
Yeo Boh Shin <110957811+YeoBohShin@users.noreply.github.com>
2023-06-26 18:32:07 +0800

    update css of navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -14,42 +14,47 @@ export default function NavBar() {
     };
 
     return (
-        <div className="navbar">
-        <nav>
+        <nav className="navbar">
             <ul>
                 {/* Logo that links to the home page */}
-                <li><Link to="/">
+                <li className="logo"><Link to="/">
                     <img alt="logo" 
                      src={require("../images/logo.png")} 
                      className="page-logo"/>
-                </Link></li>
-                {/* Search bar that links to the search page */}
-                <li><Link to="/search">
-                    <input type="text" 
-                        value={courseCode}
-                        onChange={handleSetCourseCode}
-                        onClick={handleResetCourseCode}
-                        className="search-bar" 
-                        placeholder="Search Courses"></input>
-                 </Link></li>
+                </Link>
+                    <h1 className="nav-header">
+                        AkillaC
+                    </h1>
+                </li>
                 {/* upload button that links to the upload page */}
-                <li><Link to="/upload">
+                <li className="item"><Link to="/upload">
                     <img alt="upload"
                     src={require("../images/upload-logo.png")}
                     className="upload-logo"/>
                  </Link></li>
+                {/* Search bar that links to the search page */}
+                <li className="item">
+                    <div className="search-div">
+                    <Link to="/search">
+                        <input type="text" 
+                            value={courseCode}
+                            onChange={handleSetCourseCode}
+                            onClick={handleResetCourseCode}
+                            className="search-bar" 
+                            placeholder="Search Courses"></input>
+                    </Link>
+                    <img alt="search" 
+                        src={require("../images/search-logo.png")} 
+                        className="search-button"/>
+                    </div>
+                </li>
+                {/* Button that logout */}
+                <li className="item">
+                    <button className = "logout-button" onClick = {handleLogOutClick}>
+                        LOGOUT
+                    </button>
+                </li>
             </ul>
         </nav>
-            <h1 className="header">
-                AkillaC
-            </h1>
-                <img alt="search" 
-                src={require("../images/search-logo.png")} 
-                className="search-button"/>
-            {/* Button that logout */}
-            <button className = "logout-button" onClick = {handleLogOutClick}>
-                LOGOUT
-            </button>
-        </div>
     );
 }
